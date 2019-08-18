ee095a3bad17b037004535f3d394d1c332effc40
dillonmchenry <dillon.mchenry7@gmail.com>
2019-08-18 17:48:53 -0400

    added bg

--- src/com/guitarhero/component/GamePanel.java
+++ src/com/guitarhero/component/GamePanel.java
@@ -23,7 +23,11 @@ public class GamePanel {
     public static void createGamePanel(JPanel game) {
     	game.setLayout(new BoxLayout(game, BoxLayout.Y_AXIS));
         game.setPreferredSize(new Dimension(700,800));
-        game.setBackground(Color.BLACK);
+        game.setBackground(new Color(11,11,15));
+        JLabel imageLabel = new JLabel(new ImageIcon("resources/game_bg.png"));
+        game.add(imageLabel);
+        
+        
 
         
     }
